818b270ecf7f889eb50c49b25c25da5962798297
ShoStream <contact@shostream.com>
2014-05-10 16:50:44 +0200

    added movie json generator + sample

---
+++ api/generator_movie.js
@@ -0,0 +1,53 @@
+{
+	id: '{{index()}}',
+	originalTitle: '{{firstName()}} {{lorem(4, "words")}}',
+	duration: '{{random(22,32,52)}}',
+	poster: 'http://placehold.it/320x180',
+	productionYear: '{{integer(2000,2015)}}',
+	locale_lang: '{{random("fr", "en")}}',
+	locale_data: {
+			title: '{{surname()}} {{lorem(2, "words")}}',
+			synopsis: '{{lorem(1, "paragraphs")}}'
+	},
+	ratings: '{{integer(30,100)}}',
+	country: {
+		code: '{{integer(1,29)}}',
+		name: '{{country()}}'
+	},
+	genre: {
+		code: '{{integer(1,29)}}',
+		name: '{{firstName()}}'
+	},
+	links: [
+		'{{repeat(1,5)}}',
+		{
+			id: '{{index()}}',
+			url: 'http://www.url.com',
+			host: 'exashare',
+			lang: '{{random("fr", "en")}}',
+			subtitles_lang: '{{random("fr", "", "en")}}'
+		}
+	],
+	data_member: {
+		watched: '{{bool()}}',
+		lists:
+		[
+			'{{repeat(1,5)}}',
+			{
+				id: '{{integer(1,29)}}',
+				name: '{{lorem(3, "words")}}',
+				present: '{{bool()}}'
+			}
+		]
+	},
+	suggestions: 
+	[
+		'{{repeat(4)}}',
+		{
+			id: '{{integer(1,29)}}',
+			title: '{{firstName()}} {{lorem(4, "words")}}',
+			poster: 'http://placehold.it/160x90'
+		}
+	],
+	views: '{{integer(100,10000)}}'
+}
