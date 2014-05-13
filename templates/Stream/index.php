1ff5f7ef6e0599c92be7820393ef325d68362461
ShoStream <contact@shostream.com>
2014-05-13 18:52:28 +0200

    Code enhancement (CSS & Html modified and cleaned + Jquery made independent)

--- templates/Stream/index.php
+++ templates/Stream/index.php
@@ -1,16 +1,104 @@
+<!DOCTYPE html>
 <html>
 	<head>
-		<?php include'/includes/head.php' ?>
-		<link rel="stylesheet" href="../Stream/css/stream.css">
+		<link rel="stylesheet" href="css/bootstrap.css">
+		<script src="js/jquery.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 		<title>
 			Stream
 		</title>
 	</head>
 	<body>
-		<!-- Warning example --> <?php $_POST['error'] = array('Your account\'s picture is to big!', 'The link you tried to add already exists!'); ?>
-		<!-- Info example --> <?php $_POST['info'] = array('To provide you with all our site\'s functions, we\'re using <a href="http://en.wikipedia.org/wiki/HTTP_cookie" target="_blank">cookies</a>.', 'By using this site, you agree the <a href="#">terms</a>.'); ?>
-		<!-- Success example --> <?php $_POST['success'] = array('Your settings have been updated!', 'Link successfully added!'); ?>
-		<?php include'includes/header.php' ?>
-		<?php include'includes/movies.php' ?>
+		<nav class="navbar navbar-default" role="navigation">
+			<div class="container-fluid">
+			<!-- Brand and toggle get grouped for better mobile display -->
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+						<span class="sr-only">
+							Toggle navigation
+						</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="#"><img src="img/logo.png"></img></a>
+				</div>
+				<!-- Collect the nav links, forms, and other content for toggling -->
+				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+					<ul class="nav navbar-nav">
+						<li class="active"><a href="#">Movies</a></li>
+						<li><a href="#">Shows</a></li>
+					</ul>
+					<ul class="nav navbar-nav navbar-right">
+						<li>
+							<form class="navbar-form " role="search">
+								<div class="form-group">
+									<input type="text" class="form-control lens" placeholder="Skyfall, Mentalist, @xiamix, ...">
+								</div>
+							</form>
+						</li>
+						<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown"><img src="img/1144_big.jpg" class="img-rounded img-small"> Xiamix <b class="caret"></b></a>
+							<ul class="dropdown-menu">
+								<li><a href="#">Settings</a></li>
+								<li><a href="#">Messages<span class="badge pull-right">1</span></a></li>
+								<li><a href="#">Contribute<span class="badge pull-right">7</span></a></li>
+								<li><a href="#">Moderate<span class="badge pull-right">2</span></a></li>
+								<li class="divider"></li>
+								<li><a href="#">Log out</a></li>
+							</ul>
+						</li>
+					</ul>
+				</div><!-- /.navbar-collapse -->
+			</div><!-- /.container-fluid -->
+		</nav>
+		<div class="container">
+			<a href="#" class="page-header-link">
+				<h3 class="page-header">
+					Mainstream
+				</h3>
+			</a>
+		</div>
+		<div class="container">
+			<a href="#"><img class="col-xs-2" src="img/Movie.jpg"></img></a>
+			<a href="#"><img class="col-xs-2" src="img/Movie.jpg"></img></a>
+			<a href="#"><img class="col-xs-2" src="img/Movie.jpg"></img></a>
+			<a href="#"><img class="col-xs-2" src="img/Movie.jpg"></img></a>
+			<a href="#"><img class="col-xs-2" src="img/Movie.jpg"></img></a>
+			<a href="#"><img class="col-xs-2" src="img/Movie.jpg"></img></a>
+		</div>
+		<br />
+		<div class="container">
+			<a href="#" class="page-header-link">
+				<h3 class="page-header">
+					New
+				</h3>
+			</a>
+		</div>
+		<div class="container">
+			<a href="#"><img class="col-xs-2" src="img/Movie.jpg"></img></a>
+			<a href="#"><img class="col-xs-2" src="img/Movie.jpg"></img></a>
+			<a href="#"><img class="col-xs-2" src="img/Movie.jpg"></img></a>
+			<a href="#"><img class="col-xs-2" src="img/Movie.jpg"></img></a>
+			<a href="#"><img class="col-xs-2" src="img/Movie.jpg"></img></a>
+			<a href="#"><img class="col-xs-2" src="img/Movie.jpg"></img></a>
+		</div>
+		<br />
+		<div class="container">
+			<a href="#" class="page-header-link">
+				<h3 class="page-header">Marvel's  Super Heroes <span class="label label-info">Special</span></h3>
+			</a>
+		</div>
+		<div class="container">
+			<a href="#"><img class="col-xs-2" src="img/Movie.jpg"></img></a>
+			<a href="#"><img class="col-xs-2" src="img/Movie.jpg"></img></a>
+			<a href="#"><img class="col-xs-2" src="img/Movie.jpg"></img></a>
+			<a href="#"><img class="col-xs-2" src="img/Movie.jpg"></img></a>
+			<a href="#"><img class="col-xs-2" src="img/Movie.jpg"></img></a>
+			<a href="#"><img class="col-xs-2" src="img/Movie.jpg"></img></a>
+		</div>
+		<br />
 	</body>
 </html>
