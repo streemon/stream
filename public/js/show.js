f84eef854851762f12db9a591bc73164e56082cc
ShoStream <contact@shostream.com>
2014-06-02 22:04:33 +0200

    Thinner style

--- public/js/show.js
+++ public/js/show.js
@@ -2,3 +2,10 @@ $(document).on("click", ".show-trigger", function(event){
     event.preventDefault();
     $(this).closest('div').next('.show-victim').toggle();
 }); 
+$(function() {
+    $('.nav a').on('click', function(){ 
+        if($('.navbar-toggle').css('display') !='none'){
+            $(".navbar-toggle").trigger( "click" );
+        }
+    });
+});
