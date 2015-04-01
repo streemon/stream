5a6757b5fa72348eb20a37fc783d0039af9f310b
ShoStream <contact@shostream.com>
2015-04-01 17:30:02 +0000

    hotfix

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var express = require('express'),
 var app = module.exports = express();
 
 // Configure all environments
-app.set('port', process.env.PORT || 3001);
+app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
